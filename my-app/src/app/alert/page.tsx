8282eedd47d1de1e8daf18806959c1a9d0294463
Aurelisa Juan <aurelisa.sindhu@gmail.com>
2025-02-23 00:47:18 -0500

    edited logo

--- my-app/src/app/alert/page.tsx
+++ my-app/src/app/alert/page.tsx
@@ -1,9 +1,18 @@
 "use client"
 
+import Image from "next/image"
+
 export default function FraudAlert() {
     return (
       <div className="min-h-screen flex flex-col items-center justify-center bg-[#D13434] text-white px-4">
         <div className="w-full max-w-sm space-y-8 text-center">
+          <Image
+            src="/warning.png" // Ensure your logo file is in the /public folder
+            alt="Banklytics Logo"
+            width={40}
+            height={40}
+            className="object-contain align-center mx-auto w-32 h-32 flex items-center justify-center mb-4"
+          />
           <h1 className="text-4xl font-bold tracking-wide">FRAUD DETECTED</h1>
   
           <div className="space-y-2 pt-16">
